5a02187411ad7cd66b7c4f8503909ec941d2150e
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-04-02 12:56:48 +0100

    Update account initialization and error handling
    Refactor account initialization and add error handling for account name.

--- banking/program.py
+++ banking/program.py
@@ -1,17 +1,22 @@
 from account import Account
 
-acc1 = Account()
+acc1 = Account("876543221", "Ted Lasso", 100)
 acc2 = Account()
 
-acc1.account_number = "876543221"
-acc1.account_name = "Ted Lasso"
-acc1.balance = 100
+# acc1.account_number = "876543221"
+# acc1.account_name = "Ted Lasso"
+# acc1.balance = 100
 
 acc2.account_number = "123456789"
-acc2.account_name = "Sadia Salleep"
+try:
+    acc2.account_name = "S"
+except Exception as e:
+    print(e)
+    acc2.account_name = "Anonymous"
 acc2.balance = 200
 
 acc1.deposit(100)
 acc2.deposit(200)
 print(f"Balance of {acc1.account_name} is {acc1.withdraw(150)}")
 print(f"Balance of {acc2.account_name} is {acc2.withdraw(450)}")
+print(acc1)
