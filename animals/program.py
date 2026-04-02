0b377cb9f002e071eab4039a79b16a7b7013a6cb
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-04-02 12:48:02 +0100

    Update program.py

--- animals/program.py
+++ animals/program.py
@@ -1,12 +1,12 @@
 from animal import Animal
 
-ani1 = Animal()
+ani1 = Animal(name="Fifi", colour="blue", limb_count=6)
 ani2 = Animal()
 
-ani1.name = "Fifi"
-ani1.colour = "blue"
-ani1.limb_count = -1
-ani1.color = 560
+# ani1.name = "Fifi"
+# ani1.colour = "blue"
+# ani1.limb_count = -1
+# ani1.color = 560
 
 ani2.name = "Fido"
 ani2.colour = "red"
@@ -15,3 +15,5 @@ print(ani2.limb_count)
 
 print(ani1.eat("fish"))
 print(ani2.eat("bananas"))
+
+print(ani1)
