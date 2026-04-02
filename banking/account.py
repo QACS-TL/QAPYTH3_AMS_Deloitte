14c1269be5b730bdfeac56c17015218ce0887f0d
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-04-02 12:57:15 +0100

    Refactor Account class with constructor and string method
    Refactor Account class to use constructor for initialization and add __str__ method for better representation.

--- banking/account.py
+++ banking/account.py
@@ -1,13 +1,20 @@
-
 class Account():
-    account_number = "00000000"
-    _account_name = "Anon"
-    _balance = 0.00
+    # account_number = "00000000"
+    _account_name = ""
+    _balance = 0
 
-    def get_account_name(self):
+    def __init__(self, account_number = "00000000", account_name="Anon", balance=0 ):
+        self.account_number = account_number
+        self.account_name = account_name
+        self.balance = balance
+
+
+    @property
+    def account_name(self):
         return self._account_name
 
-    def set_account_name(self, value):
+    @account_name.setter
+    def account_name(self, value):
         if len(value) < 2:
             raise ValueError("Account name must be at least 2 characters long")
         self._account_name = value
@@ -35,3 +42,6 @@ class Account():
             return self.balance
         self.balance -= amount
         return self.balance
+
+    def __str__(self):
+        return f"The current balance of {self.account_name} is {self._balance}"
